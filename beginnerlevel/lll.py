d5d90260dec1b6584dcbe9f1b663dbc71d7c31f2
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-02-22 16:10:59 +0530

    Update lll.py

--- beginnerlevel/lll.py
+++ beginnerlevel/lll.py
@@ -1,27 +1,7 @@
-#include <stdio.h>
- 
-int main()
-{
-    int     num;
-    char    choice;
-    do
-    {
-        printf("Enter an integer number :");
-        scanf("%d",&num);
-  
-        if(num==0)      
-            printf("Number is three.");
-        else if(num>0)   
-            printf("Number is POSITIVE.");
-        else          
-            printf("Number is NEGATIVE.");
-  
-        printf("\n\nWant to check again (press Y/y for 'yes') :");
-        fflush(stdin);      /*to clear input buffer*/
-        scanf(" %c",&choice); /*Here is a space before %c*/
-    }while(choice=='Y' || choice=='y');
- 
-    printf("\nBye Bye!!!");
-  
-    return 0;
-}
+num = float(input("3: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
