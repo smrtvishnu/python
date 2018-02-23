8eaaac2ea87dc5dc6f55dcc69e9dfacb8a81aa91
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-02-23 10:56:54 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,15 +1,6 @@
-    def printLeapYear():
-      print("Inpur Year is a Leap Year")
-    def printNotLeapYear():
-      print("Inpur Year is not a Leap Year")
-    input_year = int(input("Enter a Year : "))
-    if input_year % 4 == 0:
-      if input_year % 100 == 0 :
-        if input_year % 400 == 0 :
-          printLeapYear()
-        else :
-          printNotLeapYear()
-      else :
-        printLeapYear()
-    else :
-      printNotLeapYear()
+
+
+st=input("Enter a string")
+n=input("No of times to repeat:")
+print(st * int(n))
+
