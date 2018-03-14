d7dddfbf00acf7a6c752d92c8eb9fe0c91055cc9
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-03-14 16:06:19 +0530

    Create str.py

---
+++ playerlevel/str.py
@@ -0,0 +1,8 @@
+>>> parts = ['Is', 'Chicago', 'Not', 'Chicago?']
+>>> ' '.join(parts)
+'Is Chicago Not Chicago?'
+>>> ','.join(parts)
+'Is,Chicago,Not,Chicago?'
+>>> ''.join(parts)
+'IsChicagoNotChicago?'
+>>>
