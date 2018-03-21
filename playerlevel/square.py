298ccbd59c824f1e2c05c793fa5e45e73629d2c0
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-03-21 15:57:57 +0530

    Create square.py

---
+++ playerlevel/square.py
@@ -0,0 +1,20 @@
+# PYTHON program to find sum of first
+# n natural numbers.
+ 
+# Returns sum of first n natural
+# numbers
+def findSum(n) :
+    sum = 0
+    x = 1
+    while x <=n :
+        sum = sum + x
+        x = x + 1
+    return sum
+ 
+ 
+# Driver code
+ 
+n = 5
+print findSum(n)
+ 
+# This code is contributed by Nikita Tiwari.
