56fdcf25bb63c28a9ede3f58c19ee68794019f43
smrtvishnu <36722894+smrtvishnu@users.noreply.github.com>
2018-03-27 16:00:39 +0530

    Create perfect.py

---
+++ playerlevel/perfect.py
@@ -0,0 +1,12 @@
+def is_perfect_square(n):
+    x = n // 2
+    y = set([x])
+    while x * x != n:
+        x = (x + (n // x)) // 2
+        if x in y: return False
+        y.add(x)
+    return True
+
+print(is_perfect_square(8))
+print(is_perfect_square(9))
+print(is_perfect_square(100))
